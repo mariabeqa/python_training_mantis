0a5aa3969fa36bb378bda735342ab5cfdcf66c8b
mo.murashkina <mariya.murashkina@domrf.ru>
2026-07-26 15:05:06 +0600

    Добавить тест на логин

---
+++ fixture/application.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+
+from fixture.session import SessionHelper
+
+class Application:
+
+    def __init__(self, browser, base_url):
+
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser: %s" % browser)
+        self.wd.implicitly_wait(10)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False                    
+
+
+    def destroy(self):
+        self.wd.quit()
---
+++ fixture/session.py
@@ -0,0 +1,56 @@
+from selenium.webdriver.common.by import By
+
+
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element(By.CSS_SELECTOR, "span.user-info").click()
+        wd.find_element(By.LINK_TEXT, "logout").click()
+
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            wd.find_element(By.CSS_SELECTOR, "span.user-info").click()
+            wd.find_element(By.LINK_TEXT, "logout").click()
+
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        wd.find_element(By.CSS_SELECTOR, "span.user-info").click()
+        return len(wd.find_elements(By.LINK_TEXT,"logout")) > 0
+
+
+    def is_logged_in_as(self, username):
+        return self.get_logged_user() == username
+
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element(By.CSS_SELECTOR, "span.user-info").text
+
+    def login(self, username, password):
+        wd = self.app.wd
+        wd.get(self.app.base_url)
+        wd.find_element(By.NAME, "username").click()
+        wd.find_element(By.NAME, "username").clear()
+        wd.find_element(By.NAME, "username").send_keys(username)
+        wd.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+        wd.find_element(By.NAME, "password").click()
+        wd.find_element(By.NAME, "password").clear()
+        wd.find_element(By.NAME, "password").send_keys(password)
+        wd.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+
+
+    def ensure_login(self, username, password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
